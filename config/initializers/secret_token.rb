419672be61d04d061aed17549b01cb0ed93cba6e
Michael Segal <michaelsegal72@gmail.com>
2013-11-17 14:33:39 -0500

    creating the rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mappr::Application.config.secret_key_base = 'b25c9b1f201cc90f049c8797e12ec14474dcc5803f039c650b6876c0ed655803ead07dd0dd2ec1e5cc409788d8e4aa33d4fa0bc9cc5a8282e2901c750855863a'

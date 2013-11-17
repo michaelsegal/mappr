ebb1702902ec14376f8fdfb18b8205ab99b13378
Michael Segal <michaelsegal72@gmail.com>
2013-11-17 15:57:01 -0500

    buncha stuff

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,7 +8,21 @@ end
 
 
 def new
+	@place=Place.new
 end
 
+
+def create
+	@place = Place.create ( place_params)
+	redirect_to root_path
+end
+
+private
+
+def place_params
+
+params.require(:place).permit( :title, :address)
+
+end
 
 end
